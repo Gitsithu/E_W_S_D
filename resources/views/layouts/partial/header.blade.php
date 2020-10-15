c86cdc0066f68184684aeeffc2171b52f6b8e82f
Gitsithu <stns9881@gmail.com>
2020-10-15 15:38:54 +0630

    Frontend and Backend the whole module

--- resources/views/layouts/partial/header.blade.php
+++ resources/views/layouts/partial/header.blade.php
@@ -187,10 +187,10 @@
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="/backend/report/comment"><i class="fas fa-newspaper"></i>Contribution per comment Report List</a>
+                                <a class="nav-link" href="/backend/report/comment"><i class="fas fa-newspaper"></i>Contribution per without comment Report List</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/backend/report/after_comment"><i class="fas fa-newspaper"></i>Contribution per After 14 days comment Report List</a>
+                                <a class="nav-link" href="/backend/report/after_comment"><i class="fas fa-newspaper"></i>Contribution per without after 14 days comment Report List</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="/backend/log/activities"><i class="fas fa-chart-line"></i>Anatylic of Use</a>
